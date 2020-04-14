ad2b3f490ba9eeb557d3775c7953945dcd32550e
Paweł Uszakow <51965778+uszakow@users.noreply.github.com>
2020-04-14 16:42:04 +0200

    build ppokemons

---
+++ precache-manifest.64a1284770b7e89bfb64e0ed730609c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e3c91fe147bfb60f141bea7d7b47883",
+    "url": "/index.html"
+  },
+  {
+    "revision": "852dbfa1fe1d00cc130a",
+    "url": "/static/css/main.5e780c6b.chunk.css"
+  },
+  {
+    "revision": "cfebfe454ad25d1090ed",
+    "url": "/static/js/2.8d687895.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8d687895.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "852dbfa1fe1d00cc130a",
+    "url": "/static/js/main.9cf06075.chunk.js"
+  },
+  {
+    "revision": "6944d2cabd7f0b9fab4b",
+    "url": "/static/js/runtime-main.b2b9ec8d.js"
+  }
+]);

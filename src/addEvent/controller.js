42909f234a1fe349acc739b395b1f41184718181
Paweł Uszakow <51965778+uszakow@users.noreply.github.com>
2020-08-16 15:27:17 +0200

    hipotrofia

--- src/addEvent/controller.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-import * as view from './view.js';
-import * as model from './model.js';
-
-const changeLabels = () => view.changeLabels();
-
-const createNewEvent = () => model.createNewEvent();
-
-export { changeLabels, createNewEvent };
